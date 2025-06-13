3a11d8b9fd8ef2769e3d6eef131930991e6d7b62
Gabriel R <gabrielrasf@hotmail.com>
2025-06-13 17:22:34 -0300

    Estrutura login create

--- Program.cs
+++ Program.cs
@@ -6,20 +6,41 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Adiciona autenticação com cookies
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.LoginPath = "/Usuarios/Login"; // Caminho para redirecionar se não estiver logado
+        options.LoginPath = "/Usuarios/Login"; // Redireciona quando não autenticado
         options.LogoutPath = "/Usuarios/Logout";
     });
-
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Adiciona autenticação e autorização no pipeline
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
+
+app.Run();
