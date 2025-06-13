8383c9250bca68d8edf22e4a0393c3f20362df6a
Gabriel R <gabrielrasf@hotmail.com>
2025-06-13 15:33:55 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Usuario.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace puc_projeto.Models
+{
+    [Table("Table-Usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome completo é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O nome de usuário é obrigatório.")]
+        public string NomeDeUsuario { get; set; }
+
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "O gênero é obrigatório.")]
+        public Genero Genero { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório.")]
+        public Estado Estado { get; set; }
+
+        [Required(ErrorMessage = "A cidade é obrigatória.")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "A data de nascimento é obrigatória.")]
+        [DataType(DataType.Date)]
+        public DateTime Nascimento { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "A confirmação de senha é obrigatória.")]
+        [DataType(DataType.Password)]
+        [Compare("Senha", ErrorMessage = "As senhas não coincidem.")]
+        [NotMapped]
+        public string ConfirmarSenha { get; set; }
+
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Genero
+    {
+        Masculino,
+        Feminino,
+        Outro
+    }
+    public enum Estado
+    {
+        Acre,
+        Alagoas,
+        Amapá,
+        Amazonas,
+        Bahia,
+        Ceará,
+        DistritoFederal,
+        EspíritoSanto,
+        Goiás,
+        Maranhão,
+        MatoGrosso,
+        MatoGrossodoSul,
+        MinasGerais,
+        Pará,
+        Paraíba,
+        Paraná,
+        Pernambuco,
+        Piauí,
+        RiodeJaneiro,
+        RioGrandedoNorte,
+        RioGrandedoSul,
+        Rondônia,
+        Roraima,
+        SantaCatarina,
+        SãoPaulo,
+        Sergipe,
+        Tocantins
+    }
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using puc_projeto.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Adiciona autenticação com cookies
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Usuarios/Login"; // Caminho para redirecionar se não estiver logado
+        options.LogoutPath = "/Usuarios/Logout";
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
